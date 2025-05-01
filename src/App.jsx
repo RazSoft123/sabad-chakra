126cfc6bd0faf32c89e6b2eca262a33f2871796b
RazSoft <razgroupgame@gmail.com>
2025-05-01 20:58:31 +0530

    Do some refactoring of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
-import TopLabel from "./components/TopLable";
 import Hint from './components/Hint';
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import EmptyCell from "./components/EmptyCell";
 import LetterCell from "./components/LetterCell";
 import SelectedCell from "./components/SelectedCell";
+import GameProvider, { GameContext } from "./components/GameProvider";
+import MainMenu from "./components/MainMenu";
+import TopLabelContainer from "./components/TopLabelContainer";
+import levelData from './data/word_data';
+import { suffleLetters } from './utils/utilities';
+
+function reducer(state, action) {
+  switch(action.type){
+    case 'game/loadNewWord':
+      
+    break;
+    default :
+
+      break;
+  }
+}
+
+const initialState = {
+  word: levelData.levels[0].words[0].word,
+  description: levelData.levels[0].words.desc,
+  levelWordPos: 0,
+  levelWordCount: levelData.levels[0].words.length,
+  letters: levelData.levels[0].words
+}
 
 function App() {
 
-  const [word, setWord] = useState("words");
-  const [showHint, setShowHint] = useState(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  // // All the work related to level
+  // // const {word, desc: description} = levelData.levels[level].words[0];
+  // const [word, setWord] = useState(levelData.levels[level].words[0].word);
+  // const [description, setDescription] = useState(levelData.levels[level].words[0].desc);
+  // const [levelWordPos, setLevelWordPos] = useState(0);
+  // const levelWordCount = levelData.levels[level].words.length;
+
   const [currentPos, setCurrentPos] = useState(0);
-  const [score, setSocre] = useState(0);
-  const [level, setLevel] = useState(1);
-  
-  const [createdWord, setCreatedWord] = useState(Array(word.length).fill(""));
-  const [letters, setLettters] = useState(suffleLetters(word.toUpperCase().split("")))
+  const [playing, setPlaying] = useState(false);
+  const [createdWord, setCreatedWord] = useState(Array(state.word.length).fill(""));
+  const [letters, setLettters] = useState(suffleLetters(state.word.toUpperCase().split("")))
 
-  // const letters = word.toUpperCase().split("");
-
-  // const createdWord = ["W","O","","",""];
   
+
+  console.log("Word is : ", state.word);
+  console.log("Desc is : ", state.description);
+  console.log("Current pos is : ", currentPos);
+
   // Check for winning condition 
   if(letters.length === currentPos){
     checkResult();
@@ -64,18 +94,6 @@ function App() {
     }
   }
 
-  // Suffling the letters 
-  function suffleLetters(letters) {
-    let suffledLetters = [...letters];
-    let j = 0;
-    for(let i = 0; i < suffledLetters.length; i++){
-      j = Math.floor(Math.random() * (suffledLetters.length - 1));
-      [suffledLetters[i], suffledLetters[j]] = [suffledLetters[j], suffledLetters[i]];
-
-    }
-    return suffledLetters;
-  }
-
   // Check the result form the array
   function checkResult() {
     console.log(`I'm Checking the result Letter: ${letters.length} and ${currentPos}`);
@@ -84,37 +102,44 @@ function App() {
       playerWord = playerWord + createdWord[i];
     }
 
-    if(playerWord.toLowerCase() === word)
+    /*
+    if(playerWord.toLowerCase() === state.word){
       console.log("Player wins");
+      setLevelWordPos(pos => pos+1);
+      console.log("Level word position ", levelWordPos);
+      initGame(levelData.levels[level].words[levelWordPos]);
+    }
     else 
-      console.log("Player loses");
+      console.log(`player loses ${playerWord.toLowerCase()} : ${word}`);
+    */
   }
 
   return (
-    <div className="flex items-center justify-center bg-linear-to-br from-primary-500 to-skyblue-500 w-full h-full">
-      <div className="max-w-[600px] w-[300px] mt-4 mb-16 h-[80%] flex flex-col items-center justify-between">
-        <div className="flex items-center justify-between w-full">
-          <TopLabel name="Level" value={level} />
-          <TopLabel name="Score" value={score} />
-          <TopLabel name="Hint" value="💡" />
+      <div className="flex items-center justify-center bg-linear-to-br from-primary-500 to-skyblue-500 w-full h-full">
+        <div className="max-w-[600px] w-[300px] mt-4 mb-16 h-[80%] flex flex-col items-center justify-between">
+          {
+            !playing ? 
+              <MainMenu setPlaying={setPlaying} /> : 
+            <>
+            <TopLabelContainer />
+            <div className="h-full mt-10">
+              <Hint hint={state.description} />
+            </div>
+
+            <div className="flex flex-col gap-4 relative">
+              <div className="flex justify-around items-center mb-3">
+                {createdWord.map((letter, index) => letter === "" ? <EmptyCell key={index} /> : <SelectedCell handleClick={handleRemoveLetter} key={index} pos={index} letter={letter} />)}
+              </div>
+
+              <div className="flex justify-around items-center mt-6">
+                {letters.map((letter, index, arr) => <LetterCell handleClick={handleAddLetter} letter={letter} key={index} letters={arr} pos={index} />)}
+              </div>
+            </div>
+          </>
+          }
+
         </div>
-
-        <div className="h-full mt-10">
-          <Hint hint="Words that that will help you to find the word in puzzle game" />
-        </div>
-
-        <div className="flex flex-col gap-4 relative">
-          <div className="flex justify-around items-center mb-3">
-            {createdWord.map((letter, index) => letter === "" ? <EmptyCell key={index} /> : <SelectedCell handleClick={handleRemoveLetter} key={index} pos={index} letter={letter} />)}
-          </div>
-
-          <div className="flex justify-around items-center mt-6">
-            {letters.map((letter, index, arr) => <LetterCell handleClick={handleAddLetter} letter={letter} key={index} letters={arr} pos={index} />)}
-          </div>
-        </div>
-
       </div>
-    </div>
   )
 }
 
--- src/components/TopLable.jsx
+++ src/components/TopLable.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react"
+import { GameContext } from "./GameProvider"
+
 export default function TopLabel({name, value}) {
+    const {score} = useContext(GameContext);
     return (
         <div className=" border-2 px-2 rounded-4xl py-[2px] border-orange-700">
             <span className="font-bold text-orange-700 mx-2" >{name}:</span>
